2def35ae49667f6e518b472a3177889a52092a8c
kalimatas <kalimatas@gmail.com>
2010-08-01 16:49:08 +0400

    delete swp amend

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-        // comment
 	require_once ('../application/bootstrap.php');
 	$configSection = getenv('PLACES_CONFIG') ? getenv('PLACES_CONFIG') : 'general';
 	$bootstrap = new Bootstrap($configSection);
