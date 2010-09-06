7f52e736f630790cc9ddf2c4f2753c9b850f384c
kalimatas <kalimatas@gmail.com>
2010-09-06 23:19:17 +0400

    changed some brackets

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,12 +1,17 @@
 <?php
 	class IndexController extends Zend_Controller_Action{
-		public function init(){
+        public function init()
+        {
 			$this->view->initvar = 'This is init Var';
 		}
-		public function preDispatch(){
+
+        public function preDispatch()
+        {
 			$this->view->prevar = 'This is pre var!';
 		}
-		public function indexAction(){
+
+        public function indexAction()
+        {
 			$this->view->title = 'Welcome';
 			$placeFinder = new Places();
 			$this->view->places = $placeFinder->fetchLatest();
